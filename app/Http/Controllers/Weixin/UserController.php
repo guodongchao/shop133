f60cfe9763c58d45e05ca9758147e7b3ef59bfb7
root <root@localhost.localdomain>
2019-06-05 08:35:11 +0000

    d

--- app/Http/Controllers/Weixin/UserController.php
+++ app/Http/Controllers/Weixin/UserController.php
@@ -12,8 +12,31 @@ class UserController extends Controller
          '/images/2.png',
          '/images/3.png',
         ];
+        $arr=[
+            [
+                "url"=>"/pages/logs/logs",
+                "images"=>"../../images/desk_publish@2x.png",
+                "content"=>"发布车源"
+            ],
+            [
+                "url"=>"/pages/logs/logs",
+                "images"=>"../../images/desk_bid@2x.png",
+                "content"=>"户准寻车"
+            ],
+            [
+                "url"=>"/pages/logs/logs",
+                "images"=>"../../images/desk_order@2x.png",
+                "content"=>"我的订单"
+            ],
+            [
+                "url"=>"/pages/logs/logs",
+                "images"=>"../../images/desk_massage@2x.png",
+                "content"=>"通知消息"
+            ],
+        ];
        $info=[
            'data'=>$data,
+           'arr' =>$arr
 
        ];
         return $info;
